7bcea193c90e910745850a19547fb64914ca9c29
Adler Matos <adler.matos@materiagris.pe>
2021-08-26 15:04:55 -0500

    desplegable

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -1,16 +1,16 @@
 class Rutas {
 
-  private base: string = 'http://localhost:5001/api/';
+  //private base: string = 'http://localhost:5001/api/';
   //private base: string = 'http://localhost:5000/api/'; //docker
 
   //private base: string = 'http://190.216.170.173/ApiLAFT_Test/api/';
- // private base: string = 'http://190.216.170.173/ApiLAFT_Dev/api/';
+  private base: string = 'http://190.216.170.173/ApiLAFT_Dev/api/';
   //private base: string = 'http://190.216.170.173/ApiLAFT/api/';
  
   //private base: string = 'http://10.10.1.56/ApiLAFT/api/';
   //private base: string = 'https://soatservicios.protectaseurity.pe/ApiLAFT/api/';
   //private basePolicy: string = 'https://localhost:5001/api/';
-    private basePolicy: string = 'http://190.216.170.173/ApiPoliza/api/';
+  private basePolicy: string = 'http://190.216.170.173/ApiPoliza/api/';
   //private basePolicy: string = 'https://soatservicios.protectasecurity.pe/ApiPoliza/api/';
   //private basePolicy: string = 'https://localhost:5001/api/';
 
