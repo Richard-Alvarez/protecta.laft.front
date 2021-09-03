3872d3978111cb2ae0cf012fa4445a96e2c8de6b
Richard-Alvarez <richard.alvarez@materiagris.pe>
2021-09-03 17:30:04 -0500

    Cambios en template-c2

--- src/app/components/responsable/templates/template-c2/template-c2.component.ts
+++ src/app/components/responsable/templates/template-c2/template-c2.component.ts
@@ -12,6 +12,7 @@ export class TemplateC2Component implements OnInit {
   @Input() Nombre:string
   @Input() Perfil:string
   @Input() arrayDataSenal:any 
+  @Input() arrayDataResultado:any 
   @Input() index:string
   @Input() item:any
   @Input() parent:PendienteInformeComponent
--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -1,10 +1,10 @@
 class Rutas {
 
-  //private base: string = 'http://localhost:5001/api/'; 
+  private base: string = 'http://localhost:5001/api/'; 
   //private base: string = 'http://localhost:5000/api/'; //docker
 
   //private base: string = 'http://190.216.170.173/ApiLAFT_Test/api/';
-  private base: string = 'http://190.216.170.173/ApiLAFT_Dev/api/';
+  //private base: string = 'http://190.216.170.173/ApiLAFT_Dev/api/';
   //  private base: string = 'http://190.216.170.173/ApiLAFT/api/';
  
   //private base: string = 'http://10.10.1.56/ApiLAFT/api/';
@@ -173,7 +173,7 @@ class Rutas {
   public urlProcessPagosManuales: string = this.base + "sbsReport/processPagosManuales";
   public urlGetListaCargo: string = this.base + "monitoreoSenales/GetListaCargo";
   public urlConsulta360: string = this.base + "monitoreoSenales/Consulta360";
-
+  public urlGetListaResultado: string = this.base + "monitoreoSenales/GetListaResultado";
   LimpiarDataGestor() {
     localStorage.removeItem('DataGuardada')
   }
