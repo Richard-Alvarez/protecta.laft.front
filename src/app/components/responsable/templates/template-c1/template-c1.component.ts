a9a8e8144f5ebe9d7916af84372b22312ef2e6ca
Richard-Alvarez <richard.alvarez@materiagris.pe>
2021-09-13 11:34:44 -0500

    Creacion de template C1

---
+++ src/app/components/responsable/templates/template-c1/template-c1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-c1',
+  templateUrl: './template-c1.component.html',
+  styleUrls: ['./template-c1.component.css']
+})
+export class TemplateC1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
