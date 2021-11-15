70d8855ce8d2a8ec9c879c75489f51de563785c3
Richard-alvarez <richard.alvarez@materiagris.pe>
2021-11-15 15:19:48 -0500

    Cambios de claudia

--- src/app/utils/config.ts
+++ src/app/utils/config.ts
@@ -4,11 +4,7 @@ class Rutas {
   //private base: string = 'http://localhost:5000/api/'; //docker
 
   //private base: string = 'http://190.216.170.173/ApiLAFT_Test/api/';
-<<<<<<< HEAD
-private base: string = 'http://190.216.170.173/ApiLAFT_Dev/api/';
-=======
   private base: string = 'http://190.216.170.173/ApiLAFT_Dev/api/';
->>>>>>> 769ad57cb12d159942535e62c9a3431930ba87c0
   //private base: string = 'http://190.216.170.173/ApiLAFT/api/';
  // private base: string = 'http://rentas.protectasecurity.pe/ApiLAFT/api/' //PRODUCCION
   //private base: string = 'http://10.10.1.56/ApiLAFT/api/';
