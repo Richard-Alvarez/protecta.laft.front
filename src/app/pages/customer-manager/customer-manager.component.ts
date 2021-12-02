32447b4f7431340f43f652a465dbed9aad20f024
GSILVA <gsilva_dev@outlook.es>
2021-12-02 14:28:35 -0500

    cambios

--- src/app/pages/customer-manager/customer-manager.component.ts
+++ src/app/pages/customer-manager/customer-manager.component.ts
@@ -1188,13 +1188,6 @@ export class CustomerManagerComponent implements OnInit {
         }
       })
     } else {
-      let data: any = {}
-      data.name = (ItemCliente.SNOM_COMPLETO).trim()
-      data.alertId = 2
-      data.periodId = this.NPERIODO_PROCESO
-      data.tipoCargaId = 2
-      data.sClient = ItemCliente.SCLIENT
-      data.nIdUsuario = this.objUsuario.idUsuario
       let respuetaService: any = await this.getBusquedaManual(ObjListaCheckSeleccionadoxNombre)
       if (respuetaService.code == 1) {
         let mensaje = respuetaService.mesaje || 'Ocurrio un error'
