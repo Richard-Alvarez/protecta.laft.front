dafa29d3212e5b38d436df5eb6b5da7410c98807
Richard-Alvarez <richard.alvarez@materiagris.pe>
2021-12-03 11:49:00 -0500

    cambios en actualizar contra

--- src/app/pages/modal-validar-correo/modal-validar-correo.component.ts
+++ src/app/pages/modal-validar-correo/modal-validar-correo.component.ts
@@ -178,7 +178,7 @@ export class ModalValidarCorreoComponent implements OnInit {
       dataEncrip.SDESENCRIP_ID = EncripID
       dataEncrip.SHASH = hash
 
-      this.userConfigService.GetUpdUsuarioEncriptado(dataEncrip)
+      await this.userConfigService.GetUpdUsuarioEncriptado(dataEncrip)
       let dataCorreo:any = {}
       dataCorreo.SEMAIL = this.correo.toLowerCase()
       dataCorreo.ID_USER = resultado.ID[0].iD_USUARIO
