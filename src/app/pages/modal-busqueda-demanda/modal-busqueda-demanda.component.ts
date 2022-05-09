4b400cdba1779572778ea52fabffe9f087d14a99
GonzaloDavidSG <gonzalo.silva@materiagris.pe>
2022-05-09 16:11:37 -0500

    ajustes busqueda demanda

--- src/app/pages/modal-busqueda-demanda/modal-busqueda-demanda.component.ts
+++ src/app/pages/modal-busqueda-demanda/modal-busqueda-demanda.component.ts
@@ -65,7 +65,9 @@ export class ModalBusquedaDemandaComponent implements OnInit {
   }
   generarPdfIndividual(item,codImg,index){
     //this.contexto.convertirPdfIndividual(item);
-    let nombre = 'ReportInvidivual' + index
+    debugger;
+    let num = ((this.currentPage - 1) * this.itemsPerPage) + index
+    let nombre = 'ReportInvidivual' + num
     this.contexto.convertirPdfIndividual(item,codImg,nombre)
   }
   generarPdfAll(){
