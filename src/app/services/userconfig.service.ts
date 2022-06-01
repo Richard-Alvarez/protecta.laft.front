38967874b87e75b1f510c46e55bb3da2d9b5eaa8
Richard-Alvarez <richard.alvarez@materiagris.pe>
2022-06-01 09:54:10 -0500

    Cambios

--- src/app/services/userconfig.service.ts
+++ src/app/services/userconfig.service.ts
@@ -2281,6 +2281,21 @@ export class UserconfigService {
 
   }
 
+  UpdateRiesgo(data: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        this.laft.post(this.core.config.rest.urlUpdateRisgo, data).subscribe(response => {
+          return resolve(response)
+        })
+      } catch (error) {
+        return reject(error)
+      }
+    })
+
+  }
+
+  
+
   GetListaGeograficaRiesgo(): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
